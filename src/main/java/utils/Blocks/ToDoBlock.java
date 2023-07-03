315c41794bc3b74ab3e1dae66cd2bf577afe41c5
Dmitriy Shubkin <d.shubkin@hh.ru>
2023-07-03 19:18:46 +0300

    add test on todo states

--- src/main/java/utils/Blocks/ToDoBlock.java
+++ src/main/java/utils/Blocks/ToDoBlock.java
@@ -9,6 +9,11 @@ public class ToDoBlock extends BaseBlock {
 
     private static final By INPUT = new By.ByTagName("input");
     private static final By COUNT = new By.ByClassName("todo-count");
+    private static final By ALL_TAB = new By.ByXPath("//a[contains(@href, '#/')]");
+    private static final By ACTIVE_TAB = new By.ByXPath("//a[contains(@href, '#/active')]");
+    private static final By COMPLETED_TAB = new By.ByXPath("//a[contains(@href, '#/completed')]");
+    private static final By TODO_LIST_CHECKBOX = new By.ByXPath("//ul[contains(@class, \"todo-list\")]/li/div/input");
+    private static final By TODO_LIST_ELEMENT = new By.ByXPath("//ul[contains(@class, \"todo-list\")]/li");
 
     public ToDoBlock(ChromeDriver driver) {
        this.driver = driver;
@@ -25,15 +30,52 @@ public class ToDoBlock extends BaseBlock {
         return this;
     }
 
+    public ToDoBlock closeTodo(int todoNumber) {
+        driver.findElements(TODO_LIST_CHECKBOX).get(todoNumber - 1).click();
+        return this;
+    }
+
     public String getText() {
         return driver.findElement(INPUT).getText();
     }
 
-    public String getTodoText(String text) {
+    public String getTodoTextByText(String text) {
         return driver.findElement(new By.ByXPath(String.format("//label[contains(text(), '%s')]", text))).getText();
+    }
+
+    public String getTodoTextByNumber(int todoNumber) {
+        return driver.findElements(TODO_LIST_ELEMENT).get(todoNumber - 1).getText();
     }
 
     public String getTodoCountText() {
         return driver.findElement(COUNT).getText();
+    }
+
+    public int getTodoCount() {
+        return driver.findElements(TODO_LIST_ELEMENT).size();
+    }
+
+    public ToDoBlock clickOnAllTodoTab() {
+        driver.findElement(ALL_TAB).click();
+        return this;
+    }
+
+    public ToDoBlock clickOnActiveTodoTab() {
+        driver.findElement(ACTIVE_TAB).click();
+        return this;
+    }
+
+    public ToDoBlock clickOnCompletedTodoTab() throws InterruptedException {
+        driver.findElement(COMPLETED_TAB).click();
+        return this;
+    }
+
+    public boolean isTabSelected(String tabName) throws Exception {
+        return switch (tabName) {
+            case "all" -> driver.findElement(ALL_TAB).getAttribute("class").contains("selected");
+            case "active" -> driver.findElement(ACTIVE_TAB).getAttribute("class").contains("selected");
+            case "completed" -> driver.findElement(COMPLETED_TAB).getAttribute("class").contains("selected");
+            default -> throw new Exception("Не указано имя вкладки!");
+        };
     }
 }
--- src/test/java/InputInfoTest.java
+++ src/test/java/InputInfoTest.java
@@ -7,14 +7,15 @@ import utils.MainPage;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class InputInfoTest {
 
     private static final String infoText1 = "kek";
     private static final String infoText2 = "kek2";
 
-    @Test(description = "Проверяем, что добавив одну заметку, она корректно работает")
-    public void checkSuccessAddingInfo() {
+    @Test(description = "Проверяем, что добавив одну заметку, она появится на вкладках all/active, и не попадет на completed")
+    public void checkSuccessAddingInfo() throws Exception {
         System.setProperty(ConfProperties.getProperty("driver"), ConfProperties.getProperty("driverPlace"));
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -26,7 +27,16 @@ public class InputInfoTest {
                 .click()
                 .addTodo(infoText1);
 
-        assertEquals(toDoBlock.getTodoText(infoText1), infoText1, "Введенный текст отличается!");
+        assertTrue(toDoBlock.isTabSelected("all"), "Дефолтная вкладка отличается!");
+        assertEquals(toDoBlock.getTodoTextByText(infoText1), infoText1, "Введенный текст отличается!");
+
+        toDoBlock.clickOnActiveTodoTab();
+        assertTrue(toDoBlock.isTabSelected("active"), "Вкладка 'active' не выбрана");
+        assertEquals(toDoBlock.getTodoCount(), 1, "Колличество активных заметок отличается!");
+
+        toDoBlock.clickOnCompletedTodoTab();
+        assertTrue(toDoBlock.isTabSelected("completed"), "Вкладка 'completed' не выбрана");
+        assertEquals(toDoBlock.getTodoCount(), 0, "Колличество завершенных заметок > 0 !");
 
         driver.quit();
     }
@@ -42,10 +52,37 @@ public class InputInfoTest {
         MainPage mainPage = new MainPage();
         ToDoBlock toDoBlock = mainPage.getToDoBlock(driver)
                 .click()
-                .addTodo(infoText1)
-                .addTodo(infoText2);
+                .addTodo(infoText1);
+        assertEquals(toDoBlock.getTodoCountText(), "1 item left", "Колличество заметок некорректное!");
 
-        assertEquals(toDoBlock.getTodoCountText(), "2 items left", "Колличество туду отличается!");
+        toDoBlock.addTodo(infoText2);
+        assertEquals(toDoBlock.getTodoCountText(), "2 items left", "Колличество заметок некорректное!");
+
+        driver.quit();
+    }
+
+    @Test(description = "Проверяем, что закрыв заметку, она появится на вкладке с закрытыми туду")
+    public void checkCorrectCloseTodo() throws Exception {
+        System.setProperty(ConfProperties.getProperty("driver"), ConfProperties.getProperty("driverPlace"));
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfProperties.getProperty("mainpage"));
+
+        MainPage mainPage = new MainPage();
+        ToDoBlock toDoBlock = mainPage.getToDoBlock(driver)
+                .click()
+                .addTodo(infoText1)
+                .addTodo(infoText2)
+                .closeTodo(2);
+
+        assertEquals(toDoBlock.getTodoTextByNumber(1), infoText1, "Текст оставшейся заметки отличается!");
+
+        toDoBlock.clickOnCompletedTodoTab();
+
+        assertTrue(toDoBlock.isTabSelected("completed"), "Вклада с закрытыми туду не выбрана!");
+        assertEquals(toDoBlock.getTodoTextByNumber(1), infoText2, "Текст закрытой заметки отличается!");
+        assertEquals(toDoBlock.getTodoCountText(), "1 item left", "Колличество заметок некорректное!");
 
         driver.quit();
     }
